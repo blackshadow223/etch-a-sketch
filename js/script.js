1e00d9b5323fc9fdf6ebb222843c87040f7de4c3
Black Shadow <mithabozdar2@gmail.com>
2024-10-14 19:09:13 +0500

    Add an Event Listener and a loop to complete the basic task

--- js/script.js
+++ js/script.js
@@ -4,3 +4,27 @@ container.style.width = `${Math.round(window.innerWidth / 3)}px`;
 container.style.height = `${Math.round(window.innerWidth / 3)}px`;
 
 
+const numberOfSquaresInOneRow = 10;
+const totalNumberOfSquares = numberOfSquaresInOneRow ** 2; // Squared
+const widthOfSquare = container.clientWidth / numberOfSquaresInOneRow;
+
+
+for (let i = 0; i < totalNumberOfSquares; ++i) {
+    const square = document.createElement("div");
+    square.setAttribute("class", "sq");
+    square.style.width = `${widthOfSquare}px`;
+    square.style.height = `${widthOfSquare}px`;
+    square.style.opacity = 0;
+    container.appendChild(square);
+}
+
+container.addEventListener("mouseover", (event) => {
+    console.log(event.target.className);
+    if (event.target.className === "sq") {
+        event.target.style.backgroundColor = "blue";
+        let opacity = +event.target.style.opacity;
+        if (opacity < 1) {
+            event.target.style.opacity = opacity + .1;
+        }
+    }
+});
