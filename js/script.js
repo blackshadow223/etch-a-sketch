71f70fa26c085efa07483526823a3aea0ac2d764
Black Shadow <mithabozdar2@gmail.com>
2024-10-14 18:14:23 +0500

    Add basic functionality to get started

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-console.log("Nothing here!");
+
+const container = document.querySelector(".container-sq");
+container.style.width = `${Math.round(window.innerWidth / 3)}px`;
+container.style.height = `${Math.round(window.innerWidth / 3)}px`;
+
+
